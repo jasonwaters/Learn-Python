b89630fce5d26e48bcc6fff6c2c81dc15d2f6a37
Jason Waters <waters@attask.com>
2012-09-06 13:28:27 -0600

    updated credentials sample

--- experiments/credentials.sample.py
+++ experiments/credentials.sample.py
@@ -1,3 +1,3 @@
-ATTASK_BASE_URL = ""  #url to attask
-USERNAME = "" #your dot8 username
-PASSWORD = "" #your dot8 password
+ATTASK_BASE_URL = ""  #url to attask (i.e. https://attask-ondemand.com)
+USERNAME = ""
+PASSWORD = ""
